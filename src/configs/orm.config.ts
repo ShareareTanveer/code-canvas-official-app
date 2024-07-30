500f5dba29a38135b78d59c533f49a4e0b61cb15
ShareareTanveer <109936024+ShareareTanveer@users.noreply.github.com>
2024-07-30 12:57:07 +0600

    ready to deploy

--- src/configs/orm.config.ts
+++ src/configs/orm.config.ts
@@ -3,11 +3,16 @@ require('dotenv/config');
 
 const dataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'sql.freedb.tech',
-  username: process.env.DB_USER || 'freedb_sharearetanveer',
-  password: process.env.DB_PASSWORD || 'PJXH$Asn7E8r!cX',
-  database: process.env.DB_NAME || 'freedb_emspassportdb',
-  port: parseInt(process.env.DB_PORT || '3306', 10),
+  // host: process.env.DB_HOST || 'sql.freedb.tech',
+  // username: process.env.DB_USER || 'freedb_sharearetanveer',
+  // password: process.env.DB_PASSWORD || 'PJXH$Asn7E8r!cX',
+  // database: process.env.DB_NAME || 'freedb_emspassportdb',
+  // port: parseInt(process.env.DB_PORT || '3306', 10),
+  host: 'sql.freedb.tech',
+  username: 'freedb_sharearetanveer',
+  password:'PJXH$Asn7E8r!cX',
+  database: 'freedb_emspassportdb',
+  port: 3306,
   charset: 'utf8',
   synchronize: true,
   entities:
